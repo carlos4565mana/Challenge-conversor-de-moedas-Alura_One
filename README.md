7047ec1c8164977fcb93760ef91fbed1165dda37
carlos4565mana <carloscal61@gmail.com>
2024-04-25 23:35:46 -0300

    README

---
+++ README.md
@@ -0,0 +1,34 @@
+<img align=left src="https://i.imgur.com/P9fjzh4.png" height=150 alt="badge-challenge">
+
+<h2 align=center>Challenge ONE Back End - Java</h2>
+
+<div align=center>
+
+<img height="80" margin="10" src="https://i.imgur.com/9Gq6RS0.png">
+</div>
+
+### Sprint 01: Crie seu próprio conversor de moeda
+
+<br> 
+
+## História
+
+Nesta oportunidade foi solicitado para nós Devs a criação de um conversor de moeda utilizando a linguagem Java. As
+características solicitadas por nosso cliente são as seguintes:
+
+O conversor de moeda deverá:
+
+- Converter de Reais a Dólar
+- Converter de Reais a Euro
+- Converter de Reais a Peso Argentino
+- Converter de Reais a Peso Colombiano
+
+Lembrando que deve ser possível também converter de forma inversa.
+
+
+## Tecnologias utilizadas
+
+- Linguagem: Java 17
+- IDE: <a href="https://www.jetbrains.com/idea/">IntelliJ IDEA</a>
+- Currency API: <a href="https://docs.awesomeapi.com.br/api-de-moedas">[Exchange Rates API](https://www.exchangerate-api.com/docs/overview)</a>
+
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import org.json.JSONObject;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -26,8 +29,10 @@ public class Main {
         codigos.put(6, "JPY");
         codigos.put(7, "PYG");
 
+
         String codigoOrigem, codigoDestino;
         double quantidade;
+
 
 
         System.out.println("-------------------------------------------------------------------");
@@ -35,7 +40,8 @@ public class Main {
         System.out.println("-------------------------------------------------------------------");
         System.out.println();
         System.out.println();
-        System.out.println("    *************** Escoha a Moeda de Origem ***************");
+
+        System.out.println("    *************** Escoha a Moeda de Origem  ( 1 - 7 )**********");
         System.out.println("\t\t\t 1 - Dollar");
         System.out.println("\t\t\t 2 - Real Brasileiro");
         System.out.println("\t\t\t 3 - Peso Argentino");
@@ -43,11 +49,13 @@ public class Main {
         System.out.println("\t\t\t 5 - Euro");
         System.out.println("\t\t\t 6 - Iene Japones");
         System.out.println("\t\t\t 7 - Guarani Paraguaio");
+
         codigoOrigem = codigos.get(sc.nextInt());
 
-        System.out.println(codigoOrigem);
 
-        System.out.println("    *************** Escoha a Moeda de Destino ***************");
+
+
+        System.out.println("    *************** Escoha a Moeda de Destino  ( 1 - 7 )**********");
         System.out.println("\t\t\t 1 - Dollar");
         System.out.println("\t\t\t 2 - Real Brasileiro");
         System.out.println("\t\t\t 3 - Peso Argentino");
@@ -55,17 +63,20 @@ public class Main {
         System.out.println("\t\t\t 5 - Euro");
         System.out.println("\t\t\t 6 - Iene Japones");
         System.out.println("\t\t\t 7 - Guarani Paraguaio");
+        //from = sc.nextInt();
+
         codigoDestino = codigos.get(sc.nextInt());
 
-        System.out.println(codigoDestino);
-
-        System.out.println("Digite a Quantidade que deseja converter :");
+        System.out.println("Digite a quantidade que deseja converter :");
         quantidade = sc.nextFloat();
+
         sendHttpGETRequest(codigoOrigem, codigoDestino, quantidade);
 
     }
 
     private static void  sendHttpGETRequest(String codigoOrigem, String codigoDestino,double quantidade) throws IOException {
+        DecimalFormat f = new DecimalFormat("00.00");
+
         String GET_URL = "https://v6.exchangerate-api.com/v6/"+API_KEY+"/pair/"+codigoOrigem+"/"+codigoDestino+"/"+quantidade;
 
         URL url = new URL(GET_URL);
@@ -83,11 +94,10 @@ public class Main {
                 response.append(inputLine);
 
             }
-            System.out.println(response);
             in.close();
             JSONObject obj = new JSONObject(response.toString());
             var con =  obj.getDouble("conversion_result");
-            System.out.println(con);
+            System.out.println("Valor de "+ f.format(quantidade) +" " + codigoOrigem + " = " + f.format(con) +" " + codigoDestino);
 
         }else{
             System.out.println("Houve uma falha durante a reguisição!!");
